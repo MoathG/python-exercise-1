32ccfecf4045b0c1b236b44f7d1bbd3fab4eaa4f
MoathG <gharibmoath@gmail.com>
2019-11-19 15:19:38 +0200

    first commit

---
+++ exercise1.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 12:35:22 2019
+
+@author: Moath
+"""
+
+#exercise 1
+moath = """
+Hello
+    Hello
+        Hello
+"""
+print(moath)
+
+
+#exercise 2
+print('orange\n'*20)
+
+
+#exercise 3
+x = float(1)
+y = float(2.8)
+z = float('3')
+w = float('4.2')
+print(x, y, z, w, sep='10')
+
+#exercise 4
+#q = int(input('first value: '))
+#w = int(input('second value: '))
+#print(q, w)
+
+#exercise 5
+#num1 = int(input('first value: '))
+#num2 = int(input('second value: '))
+#num3 = int(input('third value: '))
+#num4 = int(input('fourth value: '))
+#num5 = int(input('fifth value: '))
+#print((num1 + num2 + num3 + num4 + num5) / 5)
+
+#exercise 6
+X = 1
+Y = 2.8
+Z = 1j
+O = 'Orange'
+A = True
+print(type(X))
+print(type(Y))
+print(type(Z))
+print(type(O))
+print(type(A))
+
+
+#exercise 7
+x1,y1 = 'Orange', 1
+X1, Y1 = 100, -10
+print(x1)
+print(y1)
+print(X1)
+print(Y1)
+
+
+#exercise 8
+# you can't wrote a number in the first of the variable name 
+x5 = 10
+print(x5)
+# you can't write a $ symbol in the variable name
+X_q = 'Orange'
+print(X_q)
+AB = 14
+print(AB)
+print('10'*10)
+
+
+#exercise 11
+#print("Hello, World")
+print("Cheers, Mate!") #this is the program
+"""Testing
+The code
+"""
+
+# exercise 12
+#name = input('please give me your name: ')
+#age = int(input('please give me your age: '))
+#print((100 - age) + 2019)
+
+
+# exercise 13
+#base = float(input('please sign the base: '))
+#height = float(input('please sign the height: '))
+#print(0.5 * base * height)
+
+
+# exercise 14
+x = 11
+y = 3
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(abs(x * -1))
+print(int(x))
+print(float(x))
+print(divmod(x, y))
+print(pow(x, y))
+print(x ** y)
+print(x > y)
+print(x == y)
+print(x != y)    
+
